aabb6f5df05323716cf740f329e6f7982e066041
Javohir Rashidov <javohir52664400456@gmail.com>
2023-07-11 23:22:16 +0500

    Create #6_rekursiya_.py

---
+++ Data_structure_and_algorithms/#6_rekursiya_.py
@@ -0,0 +1,38 @@
+"""
+Created on Tue Jul 11 18:53:51 2023
+
+
+
+@author: User rashidovj
+"""
+            
+def sana(n):
+    print(n)
+    if n<=1:
+        return 
+    else :
+        sana(n-1)
+
+    
+print(sana(8))
+
+def find_faktaryal(a):
+    if a<=1 :
+        return 1
+    else :
+        return a*find_faktaryal(a-1)
+        
+
+
+print(find_faktaryal(5))
+
+
+
+
+
+
+
+
+
+
+
